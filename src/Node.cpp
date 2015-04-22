fa6cf6e4b683c585ab408e34c360eab400eb1256
Lewis <louisalexandrevalliereslavoie@gmail.com>
2015-04-21 22:34:30 -0400

    Modification de nodefinding

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,2 +1,33 @@
 
 #include "Node.h"
+
+int Node::getDistance()
+{
+	return distance;
+}
+
+void Node::setDistance(int distance)
+{
+ this->distance = distance;
+}
+
+void Node::setDistanceProbable(double distanceProbable)
+{
+	this->distanceProbable = distanceProbable;
+}
+
+double Node::getDistanceProbable()
+{
+	return distanceProbable;
+}
+
+bool Node::isGoal()
+{
+	return goal;
+}
+
+bool Node::isMarquer()
+{
+	return marquer;
+}
+
